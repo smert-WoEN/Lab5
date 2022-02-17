59339fabb1f5031ece13c45f9e8bdd1f02c0a275
DmitryDenisov <denisdmit03@gmail.com>
2022-02-17 18:48:08 +0300

    add something, need fix it

--- src/com/company/classes/Discipline.kt
+++ src/com/company/classes/Discipline.kt
@@ -6,12 +6,12 @@ import java.util.*
 import java.util.regex.PatternSyntaxException
 import kotlin.properties.Delegates
 
-class Discipline: Discipline {
-    private lateinit var name: String
-    private var lectureHours: Long? = null
-    private var practiceHours by Delegates.notNull<Int>()
-    private var selfStudyHours by Delegates.notNull<Long>()
-    private var labsCount by Delegates.notNull<Int>()
+class Discipline : Discipline {
+    override lateinit var name: String
+    override var lectureHours: Long? = null
+    override var practiceHours by Delegates.notNull<Int>()
+    override var selfStudyHours by Delegates.notNull<Long>()
+    override var labsCount by Delegates.notNull<Int>()
     private lateinit var arguments: Array<String>
 
     constructor(argument: String) {
@@ -27,10 +27,11 @@ class Discipline: Discipline {
         } catch (e: NullPointerException) {
             throw IllegalArgumentException("Error in argument: " + e.message + ".")
         }
+        this.name = "s"
     }
 
-    constructor() {
-
+    constructor(name: String, lectureHours: Long?, practiceHours: Int, selfStudyHours: Long, labsCount: Int) {
+        this.name = name
     }
 
     override fun getDiscipline(cvs: Boolean): String {
--- src/com/company/superclasses/Discipline.kt
+++ src/com/company/superclasses/Discipline.kt
@@ -1,5 +1,12 @@
 package com.company.superclasses
 
+import kotlin.properties.Delegates
+
 interface Discipline {
         fun getDiscipline(cvs: Boolean = false): String
+        var name: String
+        var lectureHours: Long?
+        var practiceHours: Int
+        var selfStudyHours: Long
+        var labsCount: Int
 }
