ebfac17f8141b1361bfb597e8afc836bb556016e
DmitriiDenisov <denisdmit03@gmail.com>
2022-03-08 13:41:52 +0300

    add gradle, again...

---
+++ src/main/kotlin/com/company/Main.kt
@@ -0,0 +1,10 @@
+package com.company
+
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val s = scanner.nextLine()
+    println("Hello, world!")
+    println(s)
+}
