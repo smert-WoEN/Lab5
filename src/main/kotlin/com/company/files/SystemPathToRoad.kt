90d95791525465e3c0116ed52324af18f2e5c6b8
DmitryDenisov <denisdmit03@gmail.com>
2022-03-28 18:27:35 +0300

    add something

--- src/main/kotlin/com/company/files/SystemPathToRoad.kt
+++ src/main/kotlin/com/company/files/SystemPathToRoad.kt
@@ -3,12 +3,7 @@ package com.company.files
 import com.company.superclasses.PathUnzip
 
 class SystemPathToRoad (private val pathToRoad: String): PathUnzip{
-    override val path: String? = System.getenv(pathToRoad)
-        get() {
-            if (field.isNullOrBlank())
-                throw IllegalArgumentException("Can't find path \"$pathToRoad\". Please create this path.")
-            else {
-                return field
-            }
-        }
+    override val path: String
+        get() = System.getenv("wfejof")
+                ?: throw IllegalArgumentException("Can't find path \"$pathToRoad\". Please create this path.")
 }
